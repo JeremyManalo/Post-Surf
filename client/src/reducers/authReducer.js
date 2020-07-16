519318c42fd53fc89f4b5e7d2557d9f567ad9288
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-07-16 00:34:08 -0700

    Fixed authReducer arguement bug

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -16,41 +16,41 @@ const initialState = {
   user: null
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
-    case USER_LOADING:
-      return {
-        ...state,
-        isLoading: false
-      };
-    case USER_LOADED:
-      return {
-        ...state,
-        isAuthenticated: true,
+  case USER_LOADING:
+    return {
+      ...state,
+      isLoading: false
+    };
+  case USER_LOADED:
+    return {
+      ...state,
+      isAuthenticated: true,
         isLoading: false,
         user: action.payload
-      };
-    case LOGIN_SUCCESS:
-    case REGISTER_SUCCESS:
-      return {
-        ...state,
-        ...action.payload,
+    };
+  case LOGIN_SUCCESS:
+  case REGISTER_SUCCESS:
+    return {
+      ...state,
+      ...action.payload,
         isAuthenticated: true,
         isLoading: false
-      };
-    case AUTH_ERROR:
-    case LOGIN_FAIL:
-    case LOGOUT_SUCCESS:
-    case REGISTER_FAIL:
-      localStorage.removeItem();
-      return {
-        ...state,
-        token: null,
+    };
+  case AUTH_ERROR:
+  case LOGIN_FAIL:
+  case LOGOUT_SUCCESS:
+  case REGISTER_FAIL:
+    localStorage.removeItem('token');
+    return {
+      ...state,
+      token: null,
         user: null,
         isAuthenticated: false,
         isLoading: false
-      };
-    default:
-      return state;
+    };
+  default:
+    return state;
   }
 }
