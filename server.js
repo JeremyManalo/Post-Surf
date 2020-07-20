7d0fc22118ba3ea3f5698488c8ee0cd1549c8fe7
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-07-20 11:14:00 -0700

    created login modal connected to redux

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const config = require("config");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
 
 const app = express();
 
@@ -9,30 +9,30 @@ const app = express();
 app.use(express.json());
 
 // DB config
-const db = process.env.MONGO_URI || config.get("mongoURI");
+const db = process.env.MONGO_URI || config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
   .connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch(err => console.log(err));
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
 
 // Use Routes
-app.use("/api/items", require("./routes/api/items"));
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
